4e01570d5fe9f79dfd1e1886d3c630ceccfa6276
VasyaBilak <vasyabilak.vb@gmail.com>
2018-03-07 15:41:32 +0200

    change js

--- js/my.js
+++ js/my.js
@@ -1,7 +1,7 @@
 function clock_form()
          {
-	 day=new Date()
-	 clock_f=day.getHours()+":"+day.getMinutes()+":"+day.getSeconds()
+	 day = new Date()
+	 clock_f = day.getHours()+":"+day.getMinutes()+":"+day.getSeconds()
 	 document.form.f_clock.value=clock_f
          id=setTimeout("clock_form()",100)
 	 }
