c4ecec52c8e975122e7e6c6df42a9f03c28602a8
VasyaBilak <vasyabilak.vb@gmail.com>
2018-03-07 11:17:58 +0200

    Add clock

--- js/my.js
+++ js/my.js
@@ -1,2 +1,7 @@
-'use strict';
-
+function clock_form()
+         {
+	 day=new Date()
+	 clock_f=day.getHours()+":"+day.getMinutes()+":"+day.getSeconds()
+	 document.form.f_clock.value=clock_f
+         id=setTimeout("clock_form()",100)
+	 }
